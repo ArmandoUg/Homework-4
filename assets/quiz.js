fcac86212eb92fe85cc6da5a84556f55ee7f433e
Armando Ugalde <uarmando15@yahoo.com>
2022-04-16 17:37:21 -0700

    added a results div and debugged count function

--- assets/quiz.js
+++ assets/quiz.js
@@ -2,6 +2,8 @@
 var counter = document.querySelector(".timer");
 // created a var for the quizcard
 var quizcardel = document.querySelector(".quizcard");
+
+var starti = document.querySelector(".startbutton");
 // added a var for count
 var timeleft = 60;
 // added a var for placement of questions
@@ -9,7 +11,7 @@ var currentquestionindex = 0;
 // created var to contain questions
 var quizquestions = [
     {
-        question: "What color is after it is cooked?",
+        question: "What color is lobster after it is cooked?",
         answers: [
             "Green",
             "Red",
@@ -47,18 +49,48 @@ var quizquestions = [
 ];
 // created a function for count
 function count() {
+    var timer = setInterval(function() {
     timeleft--;
     counter.textContent = timeleft;
     if (timeleft === 0) {
-        
+        clearInterval(timer);
     }
+}, 1000);
 }
 // created a timer
-var timer = setInterval(count, 1000);
-
+// var timer = setInterval(count, 1000);
+// added a function to show current question
 function displayquestion() {
     quizcardel.innerHTML = "";
-    var q =quizquestions[currentquestionindex];
+    var q = quizquestions[currentquestionindex];
     var qTitleEl = document.createElement("h1");
     qTitleEl.textContent = q.question;
+    quizcardel.append(qTitleEl);
+    
+    function startquiz() {
+    };
+    var answers = quizquestions.answers;
+    for (var i = 0; i < length; i++) {
+        var answer = answers[i];
+        var button = document.createElement("button");
+        // var response = document.querySelector(".quizcard)");
+        button.textContent = answer;
+        button.addEventListener("click", function (event) {
+            var selectedanswer = event.target.textContent;
+            if (selectedanswer === q.correct) {
+                var response = document.createElement("p");
+                response.textContent = "Correct!";
+                correct();
+            } else {
+                incorrect();
+            }
+            currentquestionindex++
+            displayquestion();
+        });
+        quizcardel.append(button);
+    }
 }
+starti.addEventListener("click", function() { 
+    count()
+    displayquestion()
+    }); 
